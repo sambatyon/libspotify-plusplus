de256097f8e0ec7f534a7ec5863a2a1836aecd3e
Alexander Rojas <alexander.rojas@gmail.com>
2012-09-26 19:57:48 +0200

    Adding myself to the copyright part, I usually don't do that, but the original author's name was there and I have modify the library considerably so why not

--- src/spotify/Image.hpp
+++ src/spotify/Image.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright 2011 Jim Knowler
+ *           2012 Alexander Rojas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
